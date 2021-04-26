35f682202e574921022c096d693493a8d42e6a2f
OdedAlon <oded345alon@gmail.com>
2021-04-27 01:28:41 +0300

    responsive

--- js/wikitube-controller.js
+++ js/wikitube-controller.js
@@ -1,10 +1,42 @@
 'use strict';
 
 function onInit() {
-    renderPlayer();
+    onSearch();
 }
 
-function renderPlayer() {
-    getVideo('tora')
-        .then(res => console.log(res))
+function onSearch() {
+    let key = document.querySelector('.input-search').value;
+    getVideo(key)
+        .then(list => {
+            let firstRes = list.items[0];
+            runVideo(firstRes);
+            renderList(list.items);
+        })
+    getViki(key)
+        .then(res => {
+            let title = res.query.search[0].title;
+            let snippet = res.query.search[0].snippet;
+            renderWiki(title, snippet);
+        })
+}
+
+function runVideo(video) {
+    let id = video.id.videoId;
+    document.querySelector('.video-player-container iframe').src=`https://www.youtube.com/embed/${id}`;
+}
+
+function renderList(videos) {
+    let strHtmls = videos.map(video => {
+        return `<div class="video-card">
+            <img src="${video.snippet.thumbnails.default.url}">
+            <h4>${video.snippet.title}</h4>
+        </div>`
+    })
+    document.querySelector('.videos-list').innerHTML = strHtmls.join('');
+}
+
+function renderWiki(title, snippet) {
+    let strHtml = `<h2>${title}</h2></br>
+    <p>${snippet}</p>`;
+    document.querySelector('.wiki-container').innerHTML = strHtml;
 }
--- js/wikitube-service.js
+++ js/wikitube-service.js
@@ -1,9 +1,13 @@
 'use strict';
 
-const YT_KEY = 'AIzaSyCRr58Q-DYibbf0jZdJi5VaVfzspHXC1Z4';
+const YT_KEY = 'AIzaSyBV2hxFUdYfWxqNiWLA2mlHBMZwpJ8mwOc';
 
 function getVideo(value) {
     return axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${YT_KEY}&q=${value}`).then(res => res.data);
+}
+
+function getViki(value) {
+    return axios.get(`https://en.wikipedia.org/w/api.php?&origin=*&action=query&list=search&srsearch=${value}&format=json`).then(res => res.data);
 }
 
 
